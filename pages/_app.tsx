cb04d300d6e409f84ee540a2d516a2f5c7e853cb
Ivan <vanich7@mail.ru>
2022-09-22 11:05:20 +0300

    Доработки

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
@@ -11,6 +11,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </>
   );
-}
+};
 
 export default MyApp;
--- util/hooks/is-mobile.ts
+++ util/hooks/is-mobile.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 
 export const useIsMobile = (sizeMobile = 640) => {
-  const [isMobile, setiSMobile] = useState(
-    document.documentElement.clientWidth < sizeMobile
-  );
+  const [isMobile, setiSMobile] = useState<boolean>();
 
-  const handlerSize = (event: UIEvent) => {
+  const handlerSize = () => {
     if (document.documentElement.clientWidth < sizeMobile) {
       setiSMobile(true);
     } else {
       setiSMobile(false);
     }
   };
+
+  useEffect(() => handlerSize(), []);
 
   useEffect(() => {
     window.addEventListener('resize', handlerSize);
